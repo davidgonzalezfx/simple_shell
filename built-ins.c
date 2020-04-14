66e7aba5c5aef5443db03ebb84fe1d7eb3f2e39f
davidgonzalezfx <dvdramos16@gmail.com>
2020-04-14 16:13:20 -0500

    [Fix] Library ctype removed and betty trailing whitespace

--- built-ins.c
+++ built-ins.c
@@ -88,5 +88,5 @@ void env_built_in(void)
 		write(1, environ[i], _strlen(environ[i]));
 		write(1, "\n", 1);
 		i++;
-	}	
+	}
 }
--- shell.h
+++ shell.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
